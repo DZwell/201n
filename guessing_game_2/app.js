4d6be1c2dddb95b51e8b50e11a2843473b028791
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-10-28 16:45:34 -0700

    Removed 'typeof' line

--- guessing_game_2/app.js
+++ guessing_game_2/app.js
@@ -5,9 +5,6 @@
 
     while (playing) {
         var question1 = prompt("Do I have siblings?");
-        // if (typeof question1 == "number") {
-        //     alert("Your answer must be a string!");
-        // }
         question1 = question1.toLowerCase();
         if (question1 === 'yes' || question1 === 'y') {
             alert('Correct!');
