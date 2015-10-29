f490b145895526d90706da8fabe987677f0855e5
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-10-28 22:11:11 -0700

    Refactored

--- guessing_game_2/app.js
+++ guessing_game_2/app.js
@@ -1,42 +1,42 @@
 (function () {
 
-    var playing = true;
     var points = 0;
     var question1 = "Is the Big Lebowski the greatest film ever made?";
     var question2 = "Are dogs better than cats?"
     var question3 = "Yes or no?";
-
+    var el = document.getElementById('response');
 
     function startGame(question, answer) {
         var guess = prompt(question);
         guess = guess.toLowerCase();
+        var elText1 = 'Correct!';
+        var elText2 = 'Sorry, your opinion is wrong.';
 
         if (answer == 'yes') {
             if (guess == 'yes' || guess == 'y') {
-                var el = document.getElementById('response');
-                var elText = document.createTextNode('Correct!');
-                el.appendChild(elText);
+                el.innerHTML = elText1;
+                points++;
             } else if (guess == 'no' || guess == 'n') {
-                var el = document.getElementById('response');
-                var elText = document.createTextNode('Sorry, your opinion is wrong.');
-                el.appendChild(elText);
+                el.innerHTML = elText2;
             }
-
         } else if (answer == 'no') {
             if (guess =='no' || guess == 'n') {
-                var el = document.getElementById('response');
-                var elText = document.createTextNode('Correct!');
-                el.appendChild(elText);
+                el.innerHTML = elText1;
+                points++;
             } else if(guess == 'yes' || guess == 'y') {
-                var el = document.getElementById('response');
-                var elText = document.createTextNode('Sorry, your opinion is wrong.');
-                el.appendChild(elText);
+                el.innerHTML = elText2;
             }
         };
+    };
+
+    function displayPoints() {
+        elText3 = 'You got ' + points + ' out of 3';
+        el.innerHTML = elText3;
     };
 
 startGame(question1, 'yes');
 startGame(question2, 'no');
 startGame(question3, 'yes');
+displayPoints();
 
 })();
