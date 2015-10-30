02628c99472bdedf80056673555fc3abd1150735
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-10-30 09:58:07 -0700

    Finishing render function

--- donut_shop/app.js
+++ donut_shop/app.js
@@ -5,31 +5,69 @@ var DonutShop = function(location, stats) {
     this.maxCustPH = stats.maxCustPH;
     this.avgDonutsPerCust = stats.avgDonutsPerCust;
     this.hourlyTotals = [];
-    this.dailyDonuts = [];
+    this.dailyTotals = [];
     this.randCustomers = function(minCustPH, maxCustPH) {
         return Math.floor(Math.random() * (this.maxCustPH - this.minCustPH)) + this.minCustPH;
     };
     this.hourlyDonuts = function() {
-        hourly =  this.avgDonutsPerCust * this.randCustomers();
-        this.hourlyTotals.push(hourly);
+        for (var i = 0; i < 11; i++){
+            hourly =  this.avgDonutsPerCust * this.randCustomers();
+            this.hourlyTotals.push(hourly);
+        };
     };
-    this.dailyTotal = function() {
+    this.dailyDonuts = function() {
+        var total = 0;
         for (var i = 0; i < this.hourlyTotals.length; i++) {
-            var total = 0;
             total += this.hourlyTotals[i];
         };
-        this.dailyDonuts.push(total);
+        this.dailyTotals.push(total);
     }
 };
 
+DonutShop.prototype.render = function() {
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    td.innerHTML = this.location;
+    tr.appendChild(td);
 
-var Downtown = new DonutShop('Downtown', {minCustPH: 8, maxCustPH: 43, avgDonutsPerCust: 4.5});
-var CapitolHill = new DonutShop('Capitol Hill', {minCustPH: 4, maxCustPH: 37, avgDonutsPerCust: 2});
-var SouthLakeUnion = new DonutShop('South Lake Union', {minCustPH: 9, maxCustPH: 23, avgDonutsPerCust: 6.33});
-var Wedgewood = new DonutShop('Wedgewood', {minCustPH: 2, maxCustPH: 28, avgDonutsPerCust: 1.25});
-var Ballard = new DonutShop('Ballard', {minCustPH: 8, maxCustPH: 58, avgDonutsPerCust: 3.75});
+    for(var i = 0; i < this.hourlyTotals.length; i++) {
+        td = document.createElement('td');
+        td.innerHTML = this.hourlyTotals[i];
+        tr.appendChild(td);
+    };
+
+    td = document.createElement('td');
+    td.innerHTML = this.dailyTotals;
+    tr.appendChild(td);
+    body = document.getElementById('body').appendChild(tr);
+};
 
 
 
+var downtown = new DonutShop('Downtown', {minCustPH: 8, maxCustPH: 43, avgDonutsPerCust: 4.5});
+var capitolHill = new DonutShop('Capitol Hill', {minCustPH: 4, maxCustPH: 37, avgDonutsPerCust: 2});
+var southLakeUnion = new DonutShop('South Lake Union', {minCustPH: 9, maxCustPH: 23, avgDonutsPerCust: 6.33});
+var wedgewood = new DonutShop('Wedgewood', {minCustPH: 2, maxCustPH: 28, avgDonutsPerCust: 1.25});
+var ballard = new DonutShop('Ballard', {minCustPH: 8, maxCustPH: 58, avgDonutsPerCust: 3.75});
+
+downtown.hourlyDonuts();
+downtown.dailyDonuts();
+downtown.render();
+
+capitolHill.hourlyDonuts();
+capitolHill.dailyDonuts();
+capitolHill.render();
+
+southLakeUnion.hourlyDonuts();
+southLakeUnion.dailyDonuts();
+southLakeUnion.render();
+
+wedgewood.hourlyDonuts();
+wedgewood.dailyDonuts();
+wedgewood.render();
+
+ballard.hourlyDonuts();
+ballard.dailyDonuts();
+ballard.render();
 
 
