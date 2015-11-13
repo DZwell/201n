d61211b3dc0f4e1123dce9d9137d925796fa9ab0
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-11-12 20:13:53 -0800

    Add event listener for randomizer button

--- catDog/app.js
+++ catDog/app.js
@@ -5,26 +5,32 @@ $(function() {
     function pushImages() {
         var counter = 0;
         var file;
-        while (counter < 25) {
+        while (counter <= 24) {
             file = "images/IMG_" + counter + ".jpg";
             images.push(file);
             counter++;
         };
     };
 
+    function randomizer() {
+        var randomIndex1 = Math.floor(Math.random() * 25);
+        var randomIndex2 = Math.floor(Math.random() * 25);
+        if (randomIndex1 == randomIndex2) {
+            randomIndex1 = Math.floor(Math.random() * 25);
+        }
+        img1 = images[randomIndex1];
+        img2 = images[randomIndex2];
+
+        $('#leftPic').html('<img src="' + img1 + '">');
+        $('#rightPic').html('<img src="' + img2 + '">');
+    };
+
+    $('#reset').on('click', function() {
+        randomizer();
+    });
+
     pushImages();
-
-    var randomIndex1 = Math.floor(Math.random() * 26);
-    var randomIndex2 = Math.floor(Math.random() * 26);
-    if (randomIndex1 == randomIndex2) {
-        randomIndex1 = Math.floor(Math.random() * 26);
-    }
-    img1 = images[randomIndex1];
-    img2 = images[randomIndex2];
-    console.log(img1, img2);
-
-    $('#leftPic').html('<img src="' + img1 + '">');
-    $('#rightPic').html('<img src="' + img2 + '">');
+    randomizer();
 
 
 });
